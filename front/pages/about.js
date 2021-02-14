5eb2b1e6d8c79a972db414f8c3f474c98cc7bf70
bobonbon <sbb6987@naver.com>
2021-02-14 20:36:18 +0900

    change about

--- front/pages/about.js
+++ front/pages/about.js
@@ -41,7 +41,7 @@ const About = () => {
     );
 };
 
-export const getStaticProps = wrapper.getStaticProps(async (context) => {
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
     context.store.dispatch({
         type: LOAD_USER_REQUEST,
         data: 1,
