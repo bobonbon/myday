3d2b47b9c9c14b7bee8ff5129cf3430b30557a4d
bobonbon <sbb6987@naver.com>
2021-02-15 03:14:38 +0900

    package start

--- back/app.js
+++ back/app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(cors({
-    origin: [true, 'bobonbon.xyz'],
+    origin: [true, 'bobonbon.xyz', 'http://52.79.93.22'],
     credentials: true,  // 쿠키를 같이 전달하고 싶다면 true
 }));
 //프론트에서 백으로 데이터 보낼 때
