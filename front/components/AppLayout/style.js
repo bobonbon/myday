cc5fc5db6bd7e45ffab92c7e688a95b99c8817b3
bobonbon <sbb6987@naver.com>
2021-02-16 21:06:51 +0900

    AppLayout responsive

--- front/components/AppLayout/style.js
+++ front/components/AppLayout/style.js
@@ -9,4 +9,16 @@ export const BodyWrapper = styled.div`
 
 export const ColPadding = styled(Col)`
     padding: 10px;
+
+    @media screen and (max-width: 767px){
+
+        &:first-of-type {
+            order: 1
+        }
+    
+        &:last-of-type {
+            order: 0;  
+            max-width: 300px; 
+        }
+    }
 `;
